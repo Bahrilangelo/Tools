a434ec116d7262b58c6d0e48951abfa49dde481b
bahrilangelo <bahri.official@protonmail.com>
2023-07-28 22:22:23 +0300

    Bubble Sort

---
+++ Sorting/bubble.py
@@ -0,0 +1,12 @@
+# [64, 34, 25, 12, 22, 11, 90]
+# [11, 12, 22, 25, 34, 64, 90]
+
+def sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print(arr)
+
+sort([64, 34, 25, 12, 22, 11, 90])
